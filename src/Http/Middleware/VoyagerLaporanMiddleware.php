943f4ca449e42eece4a129420a817a3934d0b30f
rizkyadi487 <rizkyadi487@gmail.com>
2020-03-31 22:01:08 +0700

    Fix Forbbiden

--- src/Http/Middleware/VoyagerLaporanMiddleware.php
+++ src/Http/Middleware/VoyagerLaporanMiddleware.php
@@ -4,7 +4,6 @@ namespace TCG\Voyager\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 use TCG\Voyager\Facades\Voyager;
 
 class VoyagerLaporanMiddleware
@@ -19,21 +18,13 @@ class VoyagerLaporanMiddleware
      */
     public function handle($request, Closure $next)
     {
-        
-        $route_name = Route::currentRouteName();
-        
         if (!Auth::guest()) {
             $user = auth()->user();
             if (isset($user->locale)) {
                 app()->setLocale($user->locale);
             }
 
-            if($user->hasPermission('browse_laporan') && $user->hasPermission($route_name))
-            {
-                return $next($request);
-            }else{
-                return abort(403);
-            }
+            return $user->hasPermission('browse_laporan') ? $next($request) : abort(403);
         }
 
         abort(403);
