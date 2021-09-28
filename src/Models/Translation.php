727df4f7c276f8d38aff512b50d19cd7504b1eff
Rizky Adi Prasetyo <rizky@jayantara.net>
2021-09-28 11:09:34 +0700

    Translation using Cacheable

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -2,10 +2,12 @@
 
 namespace TCG\Voyager\Models;
 
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Model;
 
 class Translation extends Model
 {
+    use Cachable;
     protected $table = 'translations';
 
     protected $fillable = ['table_name', 'column_name', 'foreign_key', 'locale', 'value'];
